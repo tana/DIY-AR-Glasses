15aa48417ee6c7b7c42b4d5cab08af14020f25bd
Satoshi Tanaka <overthere555@gmail.com>
2022-03-07 06:08:33 +0900

    Auto-start the Raylib program

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 FBCP=$HOME/rpi-fbcp/build/fbcp
+AR=$HOME/ar/build/ar
 
 # Wait until framebuffers become available
 while [ ! \( -e /dev/fb1 -a -e /dev/fb2 \) ]
@@ -12,7 +13,4 @@ done
 $FBCP -p 0 -s /dev/fb1 -l 0 -t 0 -w 240 -h 240 -r 90 -fv &
 $FBCP -p 0 -s /dev/fb2 -l 0 -t 0 -w 240 -h 240 -r 270 -fv &
 
-while :
-do
-	sleep 100
-done
+exec $AR
