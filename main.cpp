ddb7729b15ca1f7f193229e49867b2ea2f4f71be
Satoshi Tanaka <overthere555@gmail.com>
2022-03-07 06:08:14 +0900

    Rendering using render textures

--- main.cpp
+++ main.cpp
@@ -4,15 +4,46 @@
 const int DISPLAY_WIDTH = 240;
 const int DISPLAY_HEIGHT = 240;
 
+void draw()
+{
+  ClearBackground(BLACK);
+
+  const int div = 8;
+  for (int i = 1; i < div; ++i) {
+    // Horizontal line
+    float y = i * float(DISPLAY_HEIGHT) / div;
+    DrawLineV(raylib::Vector2(0, y), raylib::Vector2(DISPLAY_WIDTH, y), WHITE);
+
+    // Vertical line
+    float x = i * float(DISPLAY_WIDTH) / div;
+    DrawLineV(raylib::Vector2(x, 0), raylib::Vector2(x, DISPLAY_HEIGHT), WHITE);
+  }
+}
+
 int main()
 {
   raylib::Window window(2 * DISPLAY_WIDTH, DISPLAY_HEIGHT, "");
   SetTargetFPS(60);
 
-  while (!window.ShouldClose()) {
-    BeginDrawing();
+  raylib::RenderTexture textureLeft(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+  raylib::RenderTexture textureRight(DISPLAY_WIDTH, DISPLAY_HEIGHT);
 
-    ClearBackground(RAYWHITE);
+  while (!window.ShouldClose()) {
+    // Render left eye
+    textureLeft.BeginMode();
+    draw();
+    textureLeft.EndMode();
+
+    // Render right eye
+    textureRight.BeginMode();
+    draw();
+    textureRight.EndMode();
+
+    // Render to the window
+    BeginDrawing();
+    
+    DrawTexture(textureLeft.texture, 0, 0, WHITE);
+    DrawTexture(textureRight.texture, DISPLAY_WIDTH, 0, WHITE);
 
     EndDrawing();
   }
