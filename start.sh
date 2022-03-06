687aca9b2834ca4314ba9fd9b1f06564dcb07822
Satoshi Tanaka <overthere555@gmail.com>
2022-03-07 03:58:46 +0900

    Automatic startup using systemd

--- start.sh
+++ start.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
-FBCP=../rpi-fbcp/build/fbcp
+FBCP=$HOME/rpi-fbcp/build/fbcp
+
+# Wait until framebuffers become available
+while [ ! \( -e /dev/fb1 -a -e /dev/fb2 \) ]
+do
+    sleep 1
+done
 
 # Start fbcp for two framebuffers
 $FBCP -p 0 -s /dev/fb1 -l 0 -t 0 -w 240 -h 240 -r 90 -fv &
 $FBCP -p 0 -s /dev/fb2 -l 0 -t 0 -w 240 -h 240 -r 270 -fv &
+
+while :
+do
+	sleep 100
+done
