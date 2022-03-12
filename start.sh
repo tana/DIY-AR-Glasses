1be93e40c8cb846b7b77a57ca654b74e5057a698
Satoshi Tanaka <overthere555@gmail.com>
2022-03-12 17:13:03 +0900

    Fix fbcp parameter

--- start.sh
+++ start.sh
@@ -11,6 +11,6 @@ done
 
 # Start fbcp for two framebuffers
 $FBCP -p 0 -s /dev/fb1 -l 0 -t 0 -w 240 -h 240 -r 90 -fv &
-$FBCP -p 0 -s /dev/fb2 -l 0 -t 0 -w 240 -h 240 -r 270 -fv &
+$FBCP -p 0 -s /dev/fb2 -l 240 -t 0 -w 240 -h 240 -r 270 -fv &
 
 exec $AR
